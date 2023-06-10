9c3203490ef8370e9b8aa51b09c14aac217e55fb
QuentinRoussel <roussel.quentin98@gmail.com>
2023-06-10 02:52:04 +0200

    clean project

---
+++ Assets/QRCode/Scripts/Runtime/UI/Core/UICanvas.cs
@@ -0,0 +1,28 @@
+namespace QRCode.Framework
+{
+    using UnityEngine;
+
+    public class UICanvas : MonoBehaviour
+    {
+        [SerializeField] private CanvasEnum m_canvasEnum = CanvasEnum.Undifined;
+
+        private void OnEnable()
+        {
+            UI.CanvasDatabase.AddToDatabase(m_canvasEnum, this);
+        }
+
+        private void OnDisable()
+        {
+            UI.CanvasDatabase.RemoveOfDatabase(m_canvasEnum);
+        }
+    }
+
+    public enum CanvasEnum
+    {
+        Undifined = 0,
+        GameCanvas = 1,
+        LoadingScreenCanvas = 2,
+        SubtitleCanvas = 3,
+        Save = 4,
+    }
+}
